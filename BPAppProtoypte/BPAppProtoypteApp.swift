14f0360aee04620befb453d84d00c44d2bcc53ff
Naina Subherwal <nainasubherwal@gmail.com>
2025-07-26 17:56:59 -0700

    Initial Commit

---
+++ BPAppProtoypte/BPAppProtoypteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPAppProtoypteApp.swift
+//  BPAppProtoypte
+//
+//  Created by Naina Subherwal on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BPAppProtoypteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
