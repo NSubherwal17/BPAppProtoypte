c1abfbd493294ea72f6de0a690615842d38d99f3
Naina Subherwal <nainasubherwal@gmail.com>
2025-07-26 18:00:04 -0700

    Inital formating ContentView

--- BPAppProtoypte/ContentView.swift
+++ BPAppProtoypte/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        
+        
+        
+    }//body
+    
+}//struct
+
 
 #Preview {
+    
     ContentView()
-}
+    
+}//preview
